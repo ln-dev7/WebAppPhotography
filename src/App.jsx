819cdb345337636d3de066efeb5e59431f180f83
ln-dev7 <leonelngoya@gmail.com>
2022-09-21 01:52:19 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 import Home from "./Pages/Home/Home";
-import Login from "./Pages/Login/Login";
+import Login from "./Pages/Auth/Login/Login";
 import Register from "./Pages/Auth/Register/Register";
 import Photos from "./Pages/Photos/Photos";
 
