4249bdcf340f268533c89aa8c14fa210aa6eb0d7
ln-dev-mboa <leonel.ngoya@mboadigital.tech>
2022-09-22 10:45:00 +0100

    debut header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="header">
+      <div class="menu">
+        <div class="menu-header">
+          <div>
+            <img src="assets/logo-white.svg" alt="logo" />
+          </div>
+          <div>
+            <img src="assets/close-black.svg" alt="close" />
+          </div>
+        </div>
+        <ul class="menu-items">
+          <li>Photos</li>
+          <li>Videos</li>
+          <li>Editeurs/agences</li>
+          <li>Demandez une session de formation</li>
+          <li>Blog</li>
+        </ul>
+        <div className="menu-auth">
+          <div>
+            <button>Se connecter</button>
+            <button>Uploadez une photo</button>
+          </div>
+          <p>
+            Vous n’avez pas de compte ? .
+            <Link to="/register">créer un compte</Link>
+          </p>
+        </div>
+      </div>
+      <nav className="nav">
+        <img src="assets/logo-white.svg" alt="logo" />
+        <h4>
+          <span>Français</span>
+        </h4>
+        <button type="button">Devenir créateur</button>
+        <img src="assets/menu.svg" alt="menu" />
+        <span>MENU</span>
+      </nav>
+    </div>
+  );
 }
