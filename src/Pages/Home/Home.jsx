b0b1cd4963f90ab41f78aaf6cf80444fa5d5e3b2
ln-dev-mboa <leonel.ngoya@mboadigital.tech>
2022-09-22 20:42:29 +0100

    maj

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./home.scss";
 
 function Home() {
@@ -34,6 +35,65 @@ function Home() {
               <button type="button">connectez-vous maintenant</button>
               <button type="button">Devenir créateur</button>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="archives">
+        <div className="xl">
+          <div className="archives-header">
+            <h2>Collection de photos d'archives</h2>
+            <p>
+              Les photos préférées de l'année, sélectionnées par l'équipe de
+              sélection Worflow.
+            </p>
+            <div>
+              <h3>Populaires</h3>
+              <button>femme noire</button>
+              <button>mois de l’histoire</button>
+              <button>monuments historique</button>
+              <button>diversité</button>
+              <button>football feminin</button>
+              <button>populaires</button>
+              <button>images vectorielles</button>
+            </div>
+          </div>
+          <div className="archives-photos">
+            <div>
+              <img src="/assets/images/1.png" alt="photo" />
+              <img src="/assets/images/2.png" alt="photo" />
+              <img src="/assets/images/3.png" alt="photo" />
+              <img src="/assets/images/4.png" alt="photo" />
+              <img src="/assets/images/5.png" alt="photo" />
+            </div>
+            <div>
+              <img src="/assets/images/6.png" alt="photo" />
+              <img src="/assets/images/7.png" alt="photo" />
+              <img src="/assets/images/8.png" alt="photo" />
+              <img src="/assets/images/9.png" alt="photo" />
+              <img src="/assets/images/10.png" alt="photo" />
+              <img src="/assets/images/11.png" alt="photo" />
+              <img src="/assets/images/12.png" alt="photo" />
+            </div>
+          </div>
+          <Link to="/photos">
+            <button className="archives-all">
+              <span>Afficher plus</span>
+              <img src="/assets/images/arrow-right.svg" alt="arrow" />
+            </button>
+          </Link>
+        </div>
+      </div>
+      <div className="articles">
+        <div className="xl">
+          <div className="articles-header">
+            <h2>
+              Une bonne dose d’inspiration toutes les semaines, juste pour vous
+            </h2>
+            <p>Conseils et astuces tirés de notre blog.</p>
+            <button>
+              <span>Voir tout les articles</span>
+              <img src="/assets/images/arrow-right.svg" alt="arrow" />
+            </button>
           </div>
         </div>
       </div>
