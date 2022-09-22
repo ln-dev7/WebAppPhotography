330c1556f39bbf4a2a2fe6f3b68872beb2d02922
ln-dev-mboa <leonel.ngoya@mboadigital.tech>
2022-09-22 14:53:53 +0100

    hero section et menu

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,56 +6,74 @@ export default function Header() {
   const isSearchBarShow = useRef();
   useEffect(() => {
     window.addEventListener("scroll", (e) => {
-      window.scrollY > window.innerHeight
+      window.scrollY > window.innerHeight / 2
         ? isSearchBarShow.current.classList.add("show")
         : isSearchBarShow.current.classList.remove("show");
     });
   });
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
-    <div className="header">
+    <>
       <div className="header-search" ref={isSearchBarShow}>
         <div className="xl">
           <img src="assets/search.svg" alt="serch" />
           <input type="text" placeholder="Que recherchez-vous ?" />
         </div>
       </div>
-      <div className="menu">
-        <div className="menu-header">
-          <div>
+      <div className="header">
+        {isMenuOpen && (
+          <div className="menu">
+            <div className="xl">
+              <div className="menu-header">
+                <div>
+                  <img src="assets/logo-white.svg" alt="logo" />
+                </div>
+                <div onClick={toggleMenu}>
+                  <img src="assets/close-black.svg" alt="close" />
+                </div>
+              </div>
+              <ul className="menu-items">
+                <li>Photos</li>
+                <li>Videos</li>
+                <li>Editeurs/agences</li>
+                <li>Demandez une session de formation</li>
+                <li>Blog</li>
+              </ul>
+              <div className="menu-auth">
+                <div>
+                  <button>Se connecter</button>
+                  <button>Uploadez une photo</button>
+                </div>
+                <p>
+                  Vous n’avez pas de compte ?{" "}
+                  <Link to="/register">créer un compte</Link> .
+                </p>
+              </div>
+            </div>
+          </div>
+        )}
+        <nav className="nav">
+          <div className="xl">
             <img src="assets/logo-white.svg" alt="logo" />
+            <div className="nav-right">
+              <div className="nav-right__lang">
+                <img src="assets/world-white.svg" alt="world" />
+                <span>Français</span>
+              </div>
+              <div className="line"></div>
+              <button type="button">Devenir créateur</button>
+              <div onClick={toggleMenu}>
+                <img src="assets/menu.svg" alt="menu" />
+                <span>MENU</span>
+              </div>
+            </div>
           </div>
-          <div>
-            <img src="assets/close-black.svg" alt="close" />
-          </div>
-        </div>
-        <ul className="menu-items">
-          <li>Photos</li>
-          <li>Videos</li>
-          <li>Editeurs/agences</li>
-          <li>Demandez une session de formation</li>
-          <li>Blog</li>
-        </ul>
-        <div className="menu-auth">
-          <div>
-            <button>Se connecter</button>
-            <button>Uploadez une photo</button>
-          </div>
-          <p>
-            Vous n’avez pas de compte ? .
-            <Link to="/register">créer un compte</Link>
-          </p>
-        </div>
+        </nav>
       </div>
-      <nav className="nav">
-        <img src="assets/logo-white.svg" alt="logo" />
-        <h4>
-          <img src="assets/world-white.svg" alt="world" />
-          <span>Français</span>
-        </h4>
-        <button type="button">Devenir créateur</button>
-        <img src="assets/menu.svg" alt="menu" />
-        <span>MENU</span>
-      </nav>
-    </div>
+    </>
   );
 }
